574ec1ba31c52aeceb8d7284f3115f09aa66b1ef
isaackumi <isaac.kumi@ashesi.edu.gh>
2022-08-18 15:14:20 +0000

    feat: downgraded to version 3

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://137.135.99.172/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
